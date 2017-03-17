880313f4a543a46a70504676ecdb55c01f287f3d
kittysquee <kittysquee@gmail.com>
2017-03-17 16:56:48 +0000

    Changes instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,23 +12,23 @@ class DockingStation
 
   def dock(bike)
     fail 'Docking station full' if full?
-    @all_bikes << bike
+    all_bikes << bike
   end
 
   def release_bike
     fail 'No bikes available' if empty?
-    fail 'Bike broken!' if @all_bikes[-1].broken?
-    @all_bikes.pop
+    fail 'Bike broken!' if all_bikes[-1].broken?
+    all_bikes.pop
   end
 
 private
 
   def full?
-    @all_bikes.count >= @capacity
+    all_bikes.count >= capacity
   end
 
   def empty?
-    @all_bikes.empty?
+    all_bikes.empty?
   end
 
 end
